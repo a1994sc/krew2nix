8e5b9504a91f11503bbe8a7c1210c3d2c6b14441
Allen Conlon <software@conlon.dev>
2025-04-18 21:28:32 -0400

    chore: updated formatting settings

--- treefmt.nix
+++ treefmt.nix
@@ -3,8 +3,6 @@
   # keep-sorted start block=yes newline_separated=yes prefix_order=projectRootFile,
   projectRootFile = "flake.nix";
 
-  programs.deadnix.enable = true;
-
   programs.keep-sorted.enable = true;
 
   programs.mdformat.enable = true;
@@ -19,8 +17,5 @@
   programs.rufo.enable = true;
 
   programs.statix.enable = true;
-
-  # Disabled on "flake.nix" because of some false positivies.
-  settings.formatter.deadnix.excludes = [ "**/flake.nix" ];
   # keep-sorted end
 }
